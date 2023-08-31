c3528ffa63972c78b00dfe066997e71af872618a
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-01 04:00:45 +0530

    Outputting Dynamic Data and Working with Expressions in JSX

--- react-basics/expense-tracker/src/components/ExpenseItem.js
+++ react-basics/expense-tracker/src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css"
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 298.67;
   return (
     <div className="expense-item">
-      <div >March 28th 2021</div>
-      <h2 className="expense-item__description">Expense item</h2>
-      <div className="expense-item__price">$294.67</div>
+      <div >{expenseDate.toLocaleDateString()}</div>
+      <h2 className="expense-item__description">{expenseTitle}</h2>
+      <div className="expense-item__price">${expenseAmount}</div>
     </div>
   )
 }
