f97bf556062a38c10aae14062e6247b3c091f0b8
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-01 03:56:03 +0530

    Adding Basic CSS Styling

--- react-basics/expense-tracker/src/components/ExpenseItem.js
+++ react-basics/expense-tracker/src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>March 28th 2021</div>
-      <h2>Expense item</h2>
-      <div>$294.67</div>
+    <div className="expense-item">
+      <div >March 28th 2021</div>
+      <h2 className="expense-item__description">Expense item</h2>
+      <div className="expense-item__price">$294.67</div>
     </div>
   )
 }
