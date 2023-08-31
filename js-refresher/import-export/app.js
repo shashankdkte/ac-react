357b0c8638b6ed0b25a4484c77c9f25a641a958f
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-01 01:44:51 +0530

    import and export in js

---
+++ js-refresher/import-export/app.js
@@ -0,0 +1,6 @@
+import { second, apiKey } from "./utils.js";
+import secret from "./utils.js";
+
+console.log("second ->", second)
+console.log("apiKey ->", apiKey)
+console.log("secret ->", secret);
---
+++ js-refresher/import-export/utils.js
@@ -0,0 +1,3 @@
+export default "iurkd8ga0h;3hfijf0";
+export let apiKey = "myApikey";
+export let second = "valuetoShare";
